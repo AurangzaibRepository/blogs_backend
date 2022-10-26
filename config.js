c15bbb620fae7fccb9360734a127cbb1aac31651
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-10-26 22:35:57 +0500

    Specifies database parameters in config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 const config = {
     application_port: process.env.APPLICATION_PORT,
     cors_origin_url: process.env.CORS_ORIGIN_URL,
+    DB: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE,
+    }
 };
 
 module.exports = config;
