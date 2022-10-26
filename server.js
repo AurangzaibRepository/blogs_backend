e39bdd9659d0724b8f3ed2275f7e613d4e958f30
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-10-27 00:32:55 +0500

    Sync db

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const config = require('./config');
+const db = require('./app/models');
 
 const app = express();
-
 const corsOptions = {
     origin: config.cors_origin_url,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+db.sequelize.sync();
 
 app.get('/', (req, res) => {
     res.send('Base route');
