0a7e3a44495517a5319b021fd0ac948234809bcc
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-10-27 00:14:50 +0500

    Implements sequelize

---
+++ app/models/index.js
@@ -0,0 +1,13 @@
+const dbConfig = require('../config/db.config');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
