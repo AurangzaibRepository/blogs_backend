7e9892fbf7fac054fea882ea4835e7e4ddea8d6b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-10-31 22:17:54 +0500

    Specifies columns in blog model

--- app/models/blog.model.js
+++ app/models/blog.model.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
     const blog = sequelize.define('blog', {
-
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        image: Sequelize.STRING,
+        status: Sequelize.ENUM('draft', 'published'),
+        likes_count: Sequelize.INTEGER,
     });
 
     return blog;
